7271ee938b0b2d84fc588a0f8ac003e5109998cc
CUR-53 <114309130+CUR-53@users.noreply.github.com>
2023-12-08 02:46:16 +0100

    init

---
+++ scripts/dateChecker.js
@@ -0,0 +1,16 @@
+const currentDate = new Date().toLocaleDateString('en-DK', { timeZone: 'CET' });
+
+const dateChecker = document.querySelectorAll('.checker');
+const hideText = document.querySelectorAll('.calender_card-text');
+
+dateChecker.forEach((element, index) => {
+  const elementDate = element.textContent.trim();
+  const elementDateObject = new Date(`${elementDate} GMT+1000`);
+  const currentDateObject = new Date(`${currentDate} GMT+1000`);
+
+  if (elementDateObject <= currentDateObject) {
+    element.closest('.items').classList.add('active');
+  } else {
+    hideText[index].classList.add('show');
+  }
+});
